296a829135098b71245f1516e4ed78242eea219d
Santiago Toledano <stoledano@autentia.com>
2018-09-07 22:38:31 +0200

    Modified Path to root

--- src/main/java/com/autentia/helloworld/Controller/GoogleCloudHelloWorldController.java
+++ src/main/java/com/autentia/helloworld/Controller/GoogleCloudHelloWorldController.java
@@ -1,11 +1,12 @@
 package com.autentia.helloworld.Controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GoogleCloudHelloWorldController {
 
-    @GetMapping(value = "/hello")
+    @GetMapping(value = "/")
     public String hello(){
         return "Hi from Google Cloud";
     }
